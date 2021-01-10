9f5628799599baeabf70936b22e6c52675d0e59e
lihuacai <lihuacai168@gmail.com>
2021-01-10 16:59:01 +0800

    1.feat: 汇总区间（双指针遍历）

---
+++ src/simple/汇总区间_test.go
@@ -0,0 +1,31 @@
+package simple
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_summaryRanges(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+		{"test1", args{nums: []int{0, 1, 2, 4, 5, 7}}, []string{"0->2", "4->5", "7"}},
+		{"test2", args{nums: []int{0, 2, 3, 4, 6, 8, 9}}, []string{"0", "2->4", "6", "8->9"}},
+		{"test2", args{nums: []int{-1}}, []string{"-1"}},
+		{"test2", args{nums: []int{0}}, []string{"0"}},
+		{"test2", args{nums: []int{}}, []string{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := summaryRanges(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("summaryRanges() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
